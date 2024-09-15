c14dd36ca72ca17038b7a58a36fbde8edf63b06b
Patricio0022 <niellucsx@hotmail.com>
2024-09-15 14:06:14 -0300

    älterado

--- src/main/java/app/api/Main.java
+++ src/main/java/app/api/Main.java
@@ -1,36 +1,18 @@
 package app.api;
 
 import app.api.service.HttpConnection;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { //Exception moved
 
-        File dir = new File("./files");
-        File file = new File(dir, "json.json");
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
+        SpringApplication.run(Main.class, args);
 
-        HttpConnection httpConnection = new HttpConnection();
-        httpConnection.getBaseUrl("https://jsonplaceholder.typicode.com/todos");
-        HttpURLConnection con = httpConnection.getConnection();
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-             FileWriter fileWriter = new FileWriter(file)) {
-
-            String line;
-            StringBuilder response = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line).append(System.lineSeparator());
-            }
-
-            fileWriter.write(response.toString());
-        }
-
-        System.out.println(httpConnection.getStatusCode());
-    }
+    } //print
 }

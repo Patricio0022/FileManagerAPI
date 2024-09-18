1d64ca63568d967b0b2c6d62e6b61a8f03208bc7
Patricio0022 <niellucsx@hotmail.com>
2024-09-18 14:41:37 -0300

    att

--- Back-End/src/test/java/ControllerTest.java
+++ Back-End/src/test/java/ControllerTest.java
@@ -1,13 +1,20 @@
 import app.api.controller.Controller;
+import app.api.service.FileService;
+import com.sun.net.httpserver.HttpServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 
-
 public class ControllerTest {
 
 @InjectMocks
 Controller controller;
+
+@Mock
+FileService fileService;
+
+@Mock
+HttpServer httpServer;
 
 }

a1943c1edc2e2d5aa59491810d7221c727b17341
Patricio0022 <niellucsx@hotmail.com>
2024-09-18 14:39:20 -0300

    modified and added new mocks

---
+++ Back-End/src/test/java/ControllerTest.java
@@ -0,0 +1,13 @@
+import app.api.controller.Controller;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+
+
+public class ControllerTest {
+
+@InjectMocks
+Controller controller;
+
+}

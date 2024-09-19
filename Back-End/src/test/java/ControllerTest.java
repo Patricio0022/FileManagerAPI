804f7a84f84502330400ac4444ef0c73e78c8078
Patricio0022 <niellucsx@hotmail.com>
2024-09-19 09:21:31 -0300

    att

--- Back-End/src/test/java/ControllerTest.java
+++ Back-End/src/test/java/ControllerTest.java
@@ -1,11 +1,21 @@
 import app.api.controller.Controller;
 import app.api.service.FileService;
+import app.api.service.HttpConnection;
 import com.sun.net.httpserver.HttpServer;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ControllerTest {
 
 @InjectMocks
@@ -15,6 +25,13 @@ Controller controller;
 FileService fileService;
 
 @Mock
-HttpServer httpServer;
+HttpConnection httpConnection;
+
+@Before
+public void setUp() throws IOException{
+    when(httpConnection.getConnection()).thenReturn(Mockito.mock(HttpURLConnection.class));
+    when(httpConnection.getStatusCode()).thenReturn(String.valueOf(HttpURLConnection.HTTP_OK));
+}
+
 
 }

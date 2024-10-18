819fc2f70ea8c444231239b361bbea1b2894f2a6
Patricio0022 <niellucsx@hotmail.com>
2024-10-18 19:47:39 -0300

    modified

--- Front/src/components/InputButton.tsx
+++ Front/src/components/InputButton.tsx
@@ -3,23 +3,21 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export function InputButton() {
+  const [url, setUrl] = useState('');
 
-  const [url, setUrl] = useState("");
-
-  const handleChange = (event:any) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(event.target.value);
   };
 
-
-  const handleSubmit = async (event:any) => {
-    event.preventDefault(); 
-
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     try {
- 
-      const response = await fetch(`http://localhost:8080/api/files/txt?url=${encodeURIComponent(url)}`);
+      const response = await fetch(`http://localhost:8080/api/files/file?url=${encodeURIComponent(url)}`, {
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
       const data = await response.text();
-
-    
       console.log(data);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -30,7 +28,7 @@ export function InputButton() {
     <form className="flex w-full max-w-sm items-center space-x-2" onSubmit={handleSubmit}>
       <Input 
         type="text" 
-        placeholder="Write your URL" 
+        placeholder="https://jsonplaceholder.typicode.com/todos" 
         value={url}
         onChange={handleChange} 
       />

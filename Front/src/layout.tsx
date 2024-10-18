794953376d1478799efc94a59585e08e80382f6c
Patricio0022 <niellucsx@hotmail.com>
2024-10-18 19:48:38 -0300

    a

--- Front/src/layout.tsx
+++ Front/src/layout.tsx
@@ -1,22 +1,19 @@
 import './styles/main.css';
 import { AvatarIcon } from './components/AvatarIcon';
-import { InputButton } from './components/InputButton';
+import  {InputButton} from './components/InputButton'
 import { useEffect } from 'react';
 
 
 
 export function App() {
- 
   return (
-    <div className='container p-0'>
-
-      <div className='flex justify-center p-none m-none  bg-white h-[400px] w-full'>
+    <div className='container p-20'>
+     
+      <div className='flex flex-col items-center justify-center p-none m-none rounded-sm bg-white h-[750px] w-full'>
       <AvatarIcon/>
-      
+      <div className='p-7 h-[200px] flex justify-center w-[700px]'>
+      <InputButton />
       </div>
-
-      <div className='flex justify-center bg-white h-[519px] w-auto'> 
-        <InputButton/>
       </div>
 
     </div>

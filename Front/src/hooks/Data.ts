3263d3ca205a6bd48ed138d8aa93f5f75d0b7462
Patricio0022 <niellucsx@hotmail.com>
2024-10-18 19:48:19 -0300

    a

--- Front/src/hooks/Data.ts
+++ Front/src/hooks/Data.ts
@@ -2,18 +2,5 @@ import './styles/main.css';
 import axios from 'axios';
 import { useState } from 'react';
 
-const fetchData = async (apiUrl: any) => {
-    try {
-        const response = await fetch(`/api/files/txt?url=${encodeURIComponent(apiUrl)}`);
-        const data = await response.text();
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-};
 
-fetchData('https://jsonplaceholder.typicode.com/todos');
-
-
-
-
+//fetchDataTXT('');

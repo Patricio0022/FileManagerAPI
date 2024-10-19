6cd8ef35be2ac9b6a9d195365cbeb161b8775364
Patricio0022 <niellucsx@hotmail.com>
2024-10-19 15:00:08 -0300

    css

--- Front/src/components/AvatarIcon.tsx
+++ Front/src/components/AvatarIcon.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export function AvatarIcon(){
     return(
-        <Avatar className="w-50 h-[300px]"> 
+        <Avatar className="w-50 h-[300px] transition-transform duration-300 ease-in-out transform hover:scale-110 "> 
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
